d2f4dcd6c8996656ef1486cb62937185cb44d12b
wurikiji <wurikiji@gmail.com>
2018-10-30 23:35:54 +0900

    기본 UI 셋업 - #3
    https://codelabs.developers.google.com/codelabs/flutter/index.html#3

--- lib/main.dart
+++ lib/main.dart
@@ -12,3 +12,25 @@ void main() {
     ),
   );
 }
+
+class FriendlychatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+      // TODO: implement build
+      return new MaterialApp(
+        title: "Friendlyhat",
+        home: new ChatScreen(),
+      );
+    }
+}
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build (BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Friendlychat"),
+      ),
+    );
+  }
+}
