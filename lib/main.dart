83097143545dc5e4e3ebf1cbd3c2eac74636174c
wurikiji <wurikiji@gmail.com>
2018-11-02 02:25:19 +0900

    Step #4 완성 - 아이콘 테마를 전체 테마와 일치 시킴
    https://codelabs.developers.google.com/codelabs/flutter/index.html#4
    - Step #4 완료

--- lib/main.dart
+++ lib/main.dart
@@ -39,29 +39,32 @@ class ChatScreenState extends State<ChatScreen> {
 
   Widget _buildTextComposer() {
     // 새로운 위젯을 생성하되 8.0 dp 마진을 사용
-    return new Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      // 같은 열에 텍스트 필드와 send 버튼을 위치시킨다.
-      child: new Row(
-        // 한 row에 들어갈 widget들을 배열로 정의 한다.
-        children: <Widget>[
-          // 다른 widget이 쓰고 남은 크기를 재사용 하도록 Flexible 위젯으로 만든다
-          new Flexible(
-            child: new TextField(
-              controller: _textController, // text 변화 받아 들일 컨트롤러
-              onSubmitted: _handleSubmitted, // 엔터/전송 했을 때 처리할 메소드
-              decoration: new InputDecoration.collapsed( hintText: "Send a message")
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        // 같은 열에 텍스트 필드와 send 버튼을 위치시킨다.
+        child: new Row(
+          // 한 row에 들어갈 widget들을 배열로 정의 한다.
+          children: <Widget>[
+            // 다른 widget이 쓰고 남은 크기를 재사용 하도록 Flexible 위젯으로 만든다
+            new Flexible(
+              child: new TextField(
+                controller: _textController, // text 변화 받아 들일 컨트롤러
+                onSubmitted: _handleSubmitted, // 엔터/전송 했을 때 처리할 메소드
+                decoration: new InputDecoration.collapsed( hintText: "Send a message")
+              ),
             ),
-          ),
-          // 아이콘 이미지에 맞는 크기를 갖도록 Container 사용 
-          new Container(
-            margin: new EdgeInsets.symmetric(horizontal: 4.0),
-            child: new IconButton(
-              icon: new Icon(Icons.send),
-              onPressed: () => _handleSubmitted(_textController.text)
+            // 아이콘 이미지에 맞는 크기를 갖도록 Container 사용 
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                icon: new Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text)
+              )
             )
-          )
-        ]
+          ]
+        )
       )
     );
   }
